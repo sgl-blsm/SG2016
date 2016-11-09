38d0ed851478f0d666ffcebd07182f7414a4a721
sgl-blsm <651069858@qq.com>
2016-11-10 03:05:23 +0800

    美化了后台页面

--- SG2016/settings.py
+++ SG2016/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_bootstrapped_plus',
     'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
